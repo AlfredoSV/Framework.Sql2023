0f63efa9731184b683218419fae153407f8db990
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-07-24 19:40:20 -0600

    Modify SP GetPropsQuery

--- FrameworkSql/Procedures Sql/GetPropsQuery.sql
+++ FrameworkSql/Procedures Sql/GetPropsQuery.sql
@@ -5,10 +5,16 @@ BEGIN
 DECLARE @pasaDato VARCHAR(MAX)
 
 if exists(Select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'tt')
-	drop table ttb;
+	drop table tt;
+
 
 set @pasaDato = 'Select top 0 tmp.* into tt From (' + @query + ') tmp'	 
+
+EXEC(@pasaDato)
+
 
 SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
                 WHERE INFORMATION_SCHEMA.COLUMNS.TABLE_NAME = 'TT'
 END;
+
+EXEC GetPropsQuery 'SELECT * from People'
